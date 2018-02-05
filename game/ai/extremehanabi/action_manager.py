1336b4651ba4f93c8fb922127292208f9ae5a4de
Giovanni Paolini <giove.paolini@gmail.com>
2018-02-05 19:32:32 +0100

    Prima versione della rete neurale.

--- game/ai/extremehanabi/action_manager.py
+++ game/ai/extremehanabi/action_manager.py
@@ -15,13 +15,13 @@ from torch.distributions import Categorical
 
 
 from ...action import Action, PlayAction, DiscardAction, HintAction
-from ...card import Card
+from ...card import Card, CardAppearance
 
 
 class PolicyNetwork(nn.Module):
     def __init__(self):
         super(PolicyNetwork, self).__init__()
-        self.affine1 = nn.Linear(30, 128)
+        self.affine1 = nn.Linear(655, 128)
         self.affine2 = nn.Linear(128, 128)
         self.action_head = nn.Linear(128, 3)
         self.value_head = nn.Linear(128, 1)
@@ -34,18 +34,18 @@ class PolicyNetwork(nn.Module):
         x = F.relu(self.affine2(x))
         action_scores = self.action_head(x)
         state_values = self.value_head(x)
-        return F.softmax(action_scores, dim=1), state_values
+        return F.softmax(action_scores, dim=0), state_values
 
 
-SavedAction = namedtuple('SavedAction', ['log_prob', 'value'])
+SavedAction = namedtuple('SavedAction', ['log_prob', 'value', 'chosen_action'])
 
 
 class ActionManager(object):
     
     # possible actions to choose
-    PLAY = 'P'
-    DISCARD = 'D'
-    HINT = 'H'
+    PLAY = 'Play'
+    DISCARD = 'Discard'
+    HINT = 'Hint'
     ACTIONS = [PLAY, DISCARD, HINT]
     
     def __init__(self, strategy):
@@ -59,22 +59,66 @@ class ActionManager(object):
         self.full_deck = strategy.full_deck
         self.board = strategy.board
         self.knowledge = strategy.knowledge
+        self.hands = self.strategy.hands
         
         self.COLORS_TO_NUMBERS = {color: i for (i, color) in enumerate(Card.COLORS)}
         
-        self.model = PolicyNetwork()
+        if 'model' in strategy.params:
+            # use the neural network given as a parameter
+            self.model = strategy.params['model']
+        else:
+            self.model = PolicyNetwork()
+    
     
     def log(self, message):
         self.strategy.log(message)
-
-    def select_action():
-        state = torch.rand(30)  # TODO: real state definition!
+    
+    
+    def get_state(self):
+        state = []
+        
+        # deck size
+        state.append(self.strategy.deck_size * 2.0 / 55.0 - 1.0)
+        
+        # hints
+        state.append(self.strategy.hints * 2.0 / 8.0 - 1.0)
+        
+        # lives
+        state.append(self.strategy.lives * 2.0 / 3.0 - 1.0)
+        
+        # board
+        for color in Card.COLORS:
+            for i in xrange(Card.NUM_NUMBERS+1):
+                state.append(1 if self.strategy.board[color] == i else 0)
+        
+        # my hand
+        for card_pos in xrange(self.k):
+            for color in Card.COLORS:
+                for number in xrange(1, Card.NUM_NUMBERS+1):
+                    state.append(float(self.possibilities[card_pos][CardAppearance(color, number)]) / sum(self.possibilities[card_pos].itervalues()))
+        
+        # other hands
+        for i in range(self.id+1, self.num_players) + range(self.id):
+            for card_pos in xrange(self.k):
+                for color in Card.COLORS:
+                    for number in xrange(1, Card.NUM_NUMBERS+1):
+                        state.append(1 if self.hands[i][card_pos].matches(color, number) else 0)
+                
+                # knowledge
+                state.append(1 if self.knowledge[i][card_pos].knows_exactly() or self.knowledge[i][card_pos].useless else 0)
+        
+        return torch.Tensor(state)
+    
+    
+    def select_action(self):
+        state = self.get_state()
         probs, state_value = self.model(Variable(state))
         m = Categorical(probs)
         action = m.sample()
-        model.saved_actions.append(SavedAction(m.log_prob(action), state_value))
+        chosen_action = self.ACTIONS[action.data[0]]
+        self.model.saved_actions.append(SavedAction(m.log_prob(action), state_value, chosen_action))
         # return action.data[0]
-        return ACTIONS[action]
+        return chosen_action
 
 
 
--- game/ai/extremehanabi/train.py
+++ game/ai/extremehanabi/train.py
@@ -1,5 +1,9 @@
-from collections import namedtuple
+import sys, os
+from collections import namedtuple, Counter
 
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,8 +11,8 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torch.distributions import Categorical
 
-from.action_manager import PolicyNetwork
-
+from game.ai.extremehanabi.action_manager import PolicyNetwork
+from game.game import Game
 
 Transition = namedtuple('Transition',
                         ('state', 'action', 'next_state', 'reward'))
@@ -35,9 +39,55 @@ class ReplayMemory(object):
 
 
 
+def finish_episode(model, optimizer):
+    R = 0   # reward
+    saved_actions = model.saved_actions
+    policy_losses = []
+    value_losses = []
+    rewards = []
+    for r in model.rewards[::-1]:
+        R = r + R   # gamma = 1
+        rewards.insert(0, R)
+    rewards = torch.Tensor(rewards)
+    rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
+    for (log_prob, value, _), r in zip(saved_actions, rewards):
+        reward = r - value.data[0]
+        policy_losses.append(-log_prob * reward)
+        value_losses.append(F.smooth_l1_loss(value, Variable(torch.Tensor([r]))))
+    optimizer.zero_grad()
+    loss = torch.cat(policy_losses).sum() + torch.cat(value_losses).sum()
+    loss.backward()
+    optimizer.step()
+    del model.rewards[:]
+    del model.saved_actions[:]
+
+
 if __name__ == '__main__':
     
     model = PolicyNetwork()
     optimizer = optim.Adam(model.parameters(), lr=3e-2)
-
+    
+    for i_episode in xrange(1000):
+        game = Game(num_players=5, ai="extremehanabi", ai_params={'model': model})
+        game.setup()
+        
+        previous_score = game.get_current_score()
+        
+        for player, turn in game.run_game():
+            score = game.get_current_score()
+            lives = game.lives
+            
+            reward = score - previous_score
+            if lives == 0:
+                # the game is lost
+                reward -= 50
+            
+            # TODO penalizzare azioni errate?
+            
+            previous_score = score
+            model.rewards.append(reward)
+        
+        print Counter(saved_action.chosen_action for saved_action in model.saved_actions)
+        print game.statistics
+        finish_episode(model, optimizer)
 
